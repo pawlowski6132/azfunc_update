cb4482602a5fea088c14c2cfd71fd67e611b1c19
Joe Pawlowski <j.pawlowski6132@gmail.com>
2023-09-25 17:04:22 -0400

    with sql binding

--- api/HttpTrigger1_update092523/index.js
+++ api/HttpTrigger1_update092523/index.js
@@ -1,13 +1,21 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+ 
+    if (req.body && req.body.id && req.body.name) {
+        context.bindings.sqlOutput = [];
+        context.bindings.sqlOutput.push({
+            id: req.body.id,
+            name: req.body.name
+        });
+        context.res = {
+            status: 200,
+            body: "Record updated successfully"
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass an id and name in the request body"
+        };
+    }
+};
